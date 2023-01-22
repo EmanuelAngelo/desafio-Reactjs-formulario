f84b1ba6473b730a97c2ee0a28566003cd2712cc
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-01-22 14:15:46 -0300

    O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import { useState } from 'react';
 // * Não é permitido usar refs ( para controlar os inputs (useRef))
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// done - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// done - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
 // todo - Desabilite o botão de Login equanto você está executando o login.
 // todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
 // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
@@ -31,6 +31,13 @@ export default function LoginForm() {
 
   const handleSubmit = () => {
     console.log('submitted');
+
+    let values = { email: email, password: password };
+    login(values)
+      .then(() => { })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
